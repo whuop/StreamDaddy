4d969d33a868f03db18daf3d521d69536e4db237
Kristian Brodal <kristianbrodal@gmail.com>
2018-11-22 15:03:43 +0100

    Working on making meshes assets again. This has to work!

---
+++ Assets/StreamDaddy/Editor/FbxToAsset.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FbxToAsset : EditorWindow
+{
+    private Mesh mesh;
+
+    [MenuItem("Tools/FbxToAsset")]
+    static void Init()
+    {
+        FbxToAsset window = (FbxToAsset)EditorWindow.GetWindow(typeof(FbxToAsset));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        mesh = (Mesh)EditorGUILayout.ObjectField(mesh, typeof(Mesh), false);
+        if (GUILayout.Button("Create Asset"))
+        {
+            MeshFromFBX(mesh, "Assets/testmesh.asset");
+        }
+    }
+
+    private void MeshFromFBX(Mesh mesh, string outputPath)
+    {
+        Mesh newMesh = new Mesh();
+        newMesh.vertices = mesh.vertices;
+        newMesh.uv = mesh.uv;
+        newMesh.uv2 = mesh.uv2;
+        newMesh.uv3 = mesh.uv3;
+        newMesh.uv4 = mesh.uv4;
+        newMesh.normals = mesh.normals;
+        newMesh.colors = mesh.colors;
+        newMesh.tangents = mesh.tangents;
+        newMesh.subMeshCount = mesh.subMeshCount;
+        for (int subMesh = 0; subMesh < mesh.subMeshCount; ++subMesh)
+        {
+            newMesh.SetTriangles(mesh.GetTriangles(subMesh), subMesh);
+        }
+        
+        //string meshPath = m_assetPath + "Models/" + mesh.name + "_" + hashCode + ".asset";
+
+        // Create folder if not exist
+        //if (!AssetDatabase.IsValidFolder(m_assetPath + "Models"))
+        //    AssetDatabase.CreateFolder(m_assetPath.Substring(0, m_assetPath.Length - 1), "Models");
+
+        AssetDatabase.CreateAsset(newMesh, outputPath);
+    }
+}
--- Assets/StreamDaddy/Scripts/Editor/Tasks/GenerateMeshLodsTask.cs
+++ Assets/StreamDaddy/Scripts/Editor/Tasks/GenerateMeshLodsTask.cs
@@ -181,6 +181,32 @@ namespace StreamDaddy.Editor.Tasks
             return outputMesh;
         }
 
+        private Mesh SaveMeshAsAsset(Mesh mesh, string outputPath)
+        {
+            Mesh newMesh = new Mesh();
+            newMesh.vertices = mesh.vertices;
+            newMesh.uv = mesh.uv;
+            newMesh.uv2 = mesh.uv2;
+            newMesh.uv3 = mesh.uv3;
+            newMesh.uv4 = mesh.uv4;
+            newMesh.normals = mesh.normals;
+            newMesh.colors = mesh.colors;
+            newMesh.tangents = mesh.tangents;
+            newMesh.subMeshCount = mesh.subMeshCount;
+            for (int subMesh = 0; subMesh < mesh.subMeshCount; ++subMesh)
+            {
+                newMesh.SetTriangles(mesh.GetTriangles(subMesh), subMesh);
+            }
+
+            //string meshPath = m_assetPath + "Models/" + mesh.name + "_" + hashCode + ".asset";
+
+            // Create folder if not exist
+            //if (!AssetDatabase.IsValidFolder(m_assetPath + "Models"))
+            //    AssetDatabase.CreateFolder(m_assetPath.Substring(0, m_assetPath.Length - 1), "Models");
+
+            AssetDatabase.CreateAsset(newMesh, outputPath);
+        }
+
         private string ExtractFileFormatFromPath(string path)
         {
             string[] split = path.Split('.');
